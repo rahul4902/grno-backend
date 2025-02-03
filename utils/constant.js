428b534b51a8a7d47e1e9fcb1a1249afc69f23e8
rahul.cyber <rahul.cyber2023@outlook.com>
2025-02-03 11:21:40 +0530

    s

--- utils/constant.js
+++ utils/constant.js
@@ -2,11 +2,11 @@
 // export const APP_NAME = "Amzo";\
 
 // module.exports.FRONT_BASE_URL = "http://localhost:3000";
-module.exports.fileUrl = "http://localhost:5000/uploads/";
-module.exports.baseFileUrl = "http://localhost:5000/uploads/";
+// module.exports.fileUrl = "http://localhost:5000/uploads/";
+// module.exports.baseFileUrl = "http://localhost:5000/uploads/";
 module.exports.FRONT_BASE_URL = "";
-// module.exports.baseFileUrl = "https://grno-backend.onrender.com/uploads/";
-// module.exports.fileUrl = "https://campaigns.cybermatrixsolutions.com/promotionbackend/uploads/";
+module.exports.baseFileUrl = "https://grno-backend.onrender.com/uploads/";
+module.exports.fileUrl = "https://grno-backend.onrender.com/uploads/";
 module.exports.APP_NAME = "Greno";
 
 module.exports.allowedImageTypes = [
